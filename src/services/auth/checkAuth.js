c39f2d8f47f597788de0cc1cfd911ea2727a44a7
Monde Sineke <nsineke@gmail.com>
2025-09-05 11:06:42 +0200

    chore: add comments

--- src/services/auth/checkAuth.js
+++ src/services/auth/checkAuth.js
@@ -1,5 +1,10 @@
 import { load } from "../storage/storage.js";
 
+/**
+ * Checks if the user is currently logged in by verifying the presence of an access token.
+ *
+ * @returns {boolean} Returns true if an access token exists, indicating the user is logged in; otherwise, false.
+ */
 export function isUserLoggedIn() {
   const token = load("accessToken");
   return !!token; // The !! converts a value to a strict boolean
